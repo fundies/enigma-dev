14acbdc4ad0fcb642b67c059522c46be2d770f80
Greg Williamson <cheeseboy16@gmail.com>
2019-07-11 00:35:48 -0400

    wut

--- CI/build_and_run_game.sh
+++ CI/build_and_run_game.sh
@@ -8,7 +8,7 @@ if [ "$TEST_HARNESS" == true ]; then
 else
   for mode in "Debug" "Run"; 
   do
-    MODE="$mode" ./ci-build.sh
+    MODE="$mode" ../ci-build.sh
     if [ "$COMPILER" == "MinGW64" ] || [ "$COMPILER" == "MinGW32" ]; then
       xvfb-run wine $OUTPUT > >(tee -a tee logs/enigma_game.log) 2> >(tee -a tee logs/enigma_game.log >&2)
     elif [[ ! "$GRAPHICS" =~ "OpenGLES" ]] && [ "$PLATFORM" != "SDL" ] ; then
