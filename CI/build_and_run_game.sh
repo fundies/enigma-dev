ef9d7f2ae62b5d8c361dcfeb1492356ae71e9117
Greg Williamson <cheeseboy16@gmail.com>
2019-07-11 01:26:46 -0400

    emake error fixes

--- CI/build_and_run_game.sh
+++ CI/build_and_run_game.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#set -e
+set -e
 
 if [ "$TEST_HARNESS" == true ]; then
   export ASAN_OPTIONS=detect_leaks=0;
@@ -8,7 +8,7 @@ if [ "$TEST_HARNESS" == true ]; then
 else
   for mode in "Debug" "Run"; 
   do
-    ./emake -m "$mode" -x MinGW32 -p Win32 -w Win32 -a DirectSound -e "DirectShow,WindowsTouch,XInput,MediaControlInterface,FileDropper,IniFileSystem" -o "/tmp/test.exe"
+    MODE="$mode" ./ci-build.sh
     if [ "$COMPILER" == "MinGW64" ] || [ "$COMPILER" == "MinGW32" ]; then
       xvfb-run wine $OUTPUT > >(tee -a tee logs/enigma_game.log) 2> >(tee -a tee logs/enigma_game.log >&2)
     elif [[ ! "$GRAPHICS" =~ "OpenGLES" ]] && [ "$PLATFORM" != "SDL" ] ; then
